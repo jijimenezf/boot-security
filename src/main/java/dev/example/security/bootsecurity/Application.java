53a8c81c8f98e71e0c9be8b1eecd611ba3bed690
jijimenezf <jijimenezf@hotmail.com>
2024-04-24 13:32:57 -0600

    Added scope for validating JWT requests

--- src/main/java/dev/example/security/bootsecurity/Application.java
+++ src/main/java/dev/example/security/bootsecurity/Application.java
@@ -3,6 +3,7 @@ package dev.example.security.bootsecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -17,8 +18,14 @@ public class Application {
 
 	@Bean
 	SecurityFilterChain appSecurity(HttpSecurity http, ProblemDetailsAuthenticationEntryPoint entryPoint) throws Exception {
-		http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-				.oauth2ResourceServer(oauth2 -> oauth2.authenticationEntryPoint(entryPoint).jwt(Customizer.withDefaults()));
+		http.authorizeHttpRequests(authorize -> authorize
+						.requestMatchers(HttpMethod.GET,"/cashcards/**")
+						.hasAuthority("SCOPE_cashcard:read")
+						.requestMatchers("/cashcards/**")
+						.hasAuthority("SCOPE_cashcard:write")
+			.anyRequest().authenticated())
+			.oauth2ResourceServer(oauth2 ->
+					oauth2.authenticationEntryPoint(entryPoint).jwt(Customizer.withDefaults()));
 		return http.build();
 	}
 
